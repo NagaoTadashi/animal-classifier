02f526bb9e3c41b23e7919ba16b235d1bfe9b237
NagaoTadashi <tahofu89@gmail.com>
2022-10-14 09:25:37 +0900

    add cnn.py

--- gen_data.py
+++ gen_data.py
@@ -1,6 +1,33 @@
 from PIL import Image
 import os, glob
 import numpy as np
-from sklearn import cross_validation
+from sklearn import model_selection
 
 classes = ["monkey","boar","crow"]
+num_classes = len(classes)
+image_size = 50
+
+#画像の読み込み
+X = []
+y = []
+for index,class_rabel in enumerate(classes):
+    photos_dir = "./" + "images" + "/" + class_rabel
+    files = glob.glob(photos_dir + "/*.jpg")
+    for i ,file in enumerate(files):
+        if i >= 200:
+            break
+        else:
+            image = Image.open(file)
+            image = image.convert("RGB")
+            image = image.resize((image_size,image_size))
+            data = np.asarray(image)
+            X.append(data)
+            y.append(index)
+
+X = np.array(X)
+y = np.array(y)
+
+#学習データの分割
+X_train,X_test,y_train,y_test = model_selection.train_test_split(X,y)
+Xy = (X_train,X_test,y_train,y_test)
+np.save("./data.npy",Xy)
